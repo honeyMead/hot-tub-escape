e90a059cec3ac80492685ce5df63ca41d8cbc984
honeyMead <70470522+honeyMead@users.noreply.github.com>
2023-07-09 14:18:16 +0200

    implemented basic controls

---
+++ Assets/ChefController.cs
@@ -0,0 +1,90 @@
+using System;
+using UnityEngine;
+
+public class ChefController : MonoBehaviour
+{
+    public PlayerController player;
+    public Collider2D groundCollider;
+
+    private Rigidbody2D rigid;
+    private Collider2D collider2d;
+    private float speed;
+    private float jumpHeight;
+    private bool isGrounded = true;
+    private Vector2 startPosition;
+    private float playerDistanceX;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<Collider2D>();
+        startPosition = transform.position;
+        playerDistanceX = player.transform.position.x - transform.position.x;
+    }
+
+    void Update()
+    {
+        StayGrounded();
+        OnlyComeCloserWhenPlayerIsStopped();
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+        CheckIfGrounded();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var hasHitPlayer = collision.gameObject.CompareTag("Player");
+
+        if (hasHitPlayer)
+        {
+            // TODO game over
+            Debug.Log("Game Over");
+        }
+    }
+
+    private void Jump()
+    {
+        var playerHeightToCauseJump = 1f;
+        jumpHeight = player.jumpHeight;
+
+        if (isGrounded && player.transform.position.y >= playerHeightToCauseJump)
+        {
+            rigid.gravityScale = 1;
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpHeight);
+        }
+    }
+
+    private void CheckIfGrounded()
+    {
+        isGrounded = collider2d.IsTouching(groundCollider);
+    }
+
+    private void StayGrounded()
+    {
+        if (transform.position.y < startPosition.y)
+        {
+            rigid.gravityScale = 0;
+            transform.position = new Vector2(transform.position.x, startPosition.y);
+        }
+    }
+
+    private void OnlyComeCloserWhenPlayerIsStopped()
+    {
+        if (player.rigid.velocity.x <= player.speed / 4f)
+        {
+            playerDistanceX = player.transform.position.x - transform.position.x;
+            MoveRight();
+        }
+        var x = player.transform.position.x - playerDistanceX;
+        transform.position = new Vector2(x, transform.position.y);
+    }
+
+    private void MoveRight()
+    {
+        speed = player.speed;
+        rigid.velocity = new Vector2(speed, rigid.velocity.y);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+    public Camera mainCamera;
+    public GameObject ground;
+
+    public Rigidbody2D rigid { get; private set; }
+
+    private Collider2D collider2d;
+    private bool isGrounded = true;
+    private float camDistanceX;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<Collider2D>();
+        camDistanceX = mainCamera.transform.position.x - transform.position.x;
+    }
+
+    void Update()
+    {
+        CheckIfGrounded();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MoveRight();
+        MoveCamera();
+        MoveGround();
+    }
+
+    private void CheckIfGrounded()
+    {
+        var overlapColliders = new List<Collider2D>();
+        var collidersCount = collider2d.OverlapCollider(new ContactFilter2D().NoFilter(), overlapColliders);
+        isGrounded = collidersCount >= 1;
+    }
+
+    private void Jump()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, jumpHeight);
+    }
+
+    private void MoveRight()
+    {
+        rigid.velocity = new Vector2(speed, rigid.velocity.y);
+    }
+
+    private void MoveCamera()
+    {
+        var cameraPos = mainCamera.transform.position;
+        mainCamera.transform.position = new Vector3(transform.position.x + camDistanceX, cameraPos.y, cameraPos.z);
+    }
+
+    private void MoveGround()
+    {
+        var cameraPos = mainCamera.transform.position;
+        ground.transform.position = new Vector2(cameraPos.x, ground.transform.position.y);
+    }
+}
